40273750f6f930e3edde0b4b956c5964abba779c
GustavFr <sasha-yarushin@mail.ru>
2019-11-10 14:30:22 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,17 +8,19 @@ double rnd(){
 }
 
 struct till{
-    double count = 0;
+    unsigned count = 0;
     double Tk = 0;
 };
 
 static double Tz = 1; //Время обслуживания одного покупателя
-static const unsigned N = 10; //Колличество касс
-static double T = 1000; //Время работы модели
+static const unsigned N = 15; //Колличество касс
+static double T = 100; //Время работы модели
 static till tills[N]; //Массив касс
+static unsigned tillsAcc[N+1]; //Аккумулятор
 static std::vector<double> buyers; //Массив покупателей
-static double a = 1.0/20.0;
-static unsigned not_served = 0;
+static double a = 1.0/5.0; // Частота появления покупателя
+static unsigned not_served = 0; //Количество необслуженых покупателей
+static unsigned repeatCount = 10000; //Количество итераций для подсчета среднего значения
 
 void moddeling(){
     for(unsigned i = 0; i < N; ++i){
@@ -33,22 +35,41 @@ void moddeling(){
 
 }
 
+
+void clear(){
+    for(unsigned i = 0; i < N; ++i){
+        tills[i].Tk = 0;
+        tills[i].count = 0;
+    }
+    buyers.clear();
+    not_served = 0;
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
     srand(time(nullptr));
 
-    while(true){
-        buyers.push_back(-a * log(rnd()));
-        if(buyers.size() > 0) buyers[buyers.size() - 1] += buyers[buyers.size() - 2];
-        if(buyers[buyers.size() - 1] > T) break;
-        moddeling();
-    }
+    for(unsigned i = 0; i < repeatCount; ++i){
+        clear();
+        while(true){
+            buyers.push_back(-a * log(rnd()));
+            if(buyers.size() > 0) buyers[buyers.size() - 1] += buyers[buyers.size() - 2];
+            if(buyers[buyers.size() - 1] > T) break;
+            moddeling();
+        }
 
-    for(unsigned i = 0; i < N; ++i){
-        std::cout << "Till #" << (i+1) << " - " << tills[i].count << std::endl;
+        for(unsigned i = 0; i < N; ++i){
+            tillsAcc[i] += tills[i].count;
+        }
+        tillsAcc[N] += not_served;
+
+
     }
-    std::cout << "Not served: " << not_served << std::endl;
+    for(unsigned i = 0; i < N; ++i){
+        std::cout << "Till #" << (i+1) << " - " << tillsAcc[i] / (repeatCount * 1.0) << std::endl;
+    }
+    std::cout << "Not served: " << tillsAcc[N] / (repeatCount * 1.0) << std::endl;
 
     return app.exec();
 }
