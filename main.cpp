9d9d6c2465cd5506c8abd322d97699c768647d6b
GustavFr <sasha-yarushin@mail.ru>
2019-11-10 12:25:13 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,34 @@
 #include <QCoreApplication>
+#include <cmath>
+#include <iostream>
+#include <ctime>
+
+double rnd(){
+    return rand() / (RAND_MAX * 1.0);
+}
 
 struct till{
     int count = 0;
-    int Tk;
+    int Tk = 0;
 };
 
 static unsigned Tz = 2; //Время обслуживания одного покупателя
 static const unsigned N = 10; //Колличество касс
 static unsigned T = 100; //Время работы модели
 static till tills[N]; //Массив касс
-static std::vector<int> buyers;
+static std::vector<double> buyers; //Массив покупателей
+static double a = 1.0/5.0;
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    QCoreApplication app(argc, argv);
+    srand(time(nullptr));
+
+
+    for(int i = 0; i < 10; ++i)
+        std::cout << -a * log(rnd()) << std::endl;
 
 
 
-    return a.exec();
+    return app.exec();
 }
